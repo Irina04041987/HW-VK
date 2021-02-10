34cb4888b3b639143b57655ed1c5801304369fd6
Irina Platonova <crazyangeli@yandex.ru>
2021-02-10 19:47:30 +0300

    add PostData.java #v1

---
+++ src/main/java/domain/PostData.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class PostData {
+    private int postID;
+    private int AuthorID;
+    private int ownerID;
+    private int postTime;
+    private String text;
+    private int replyOwnerID;
+    private int replyPostID;
+
+    private String imageURL;
+
+    private String postType;
+}
