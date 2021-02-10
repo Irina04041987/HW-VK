9cbab6df1b2be521857e412937f720994cf4901b
Irina Platonova <crazyangeli@yandex.ru>
2021-02-10 20:44:04 +0300

    added PostManager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,24 @@
+package manager;
+
+import domain.PostData;
+
+public class PostManager {
+    private PostData[] posts;
+    PostData postData = new PostData();
+
+    public PostData[] getPostsToArray() {
+        //TO DO
+        return null;
+    }
+
+    public PostData[] search(int ownerID) {
+        //TO DO
+        return null;
+    }
+
+    public void delete(int postID, int ownerID) {
+        //TO DO
+    }
+
+
+}
